ab32604760065e9dcc76ef60ac3d6ca245ca44a0
Costin Leau <costin.leau@gmail.com>
2020-05-13 15:08:56 +0300

    QL utils project not being seen by eql:qa:consumer project

---
+++ x-pack/plugin/eql/qa/consumer/src/main/java/org/elasticsearch/xpack/plugin/eql/qa/consumer/Consumer.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.plugin.eql.qa.consumer;
+
+import org.elasticsearch.xpack.plugin.ql.qa.utils.Utils;
+
+public class Consumer {}
