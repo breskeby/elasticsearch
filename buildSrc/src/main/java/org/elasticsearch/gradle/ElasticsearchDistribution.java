7e89d3cc3d933e8d9e209d35299a780603a0752c
Rene Groeschke <rene@elastic.co>
2020-05-19 19:50:20 +0200

    Revert "Try workaround of tc init script failing to resolve configuration"
    This reverts commit 29c16ae841caf7cf240eb7de22c3c871ee1d9fcc.

--- buildSrc/src/main/java/org/elasticsearch/gradle/ElasticsearchDistribution.java
+++ buildSrc/src/main/java/org/elasticsearch/gradle/ElasticsearchDistribution.java
@@ -208,27 +208,9 @@ public class ElasticsearchDistribution implements Buildable, Iterable<File> {
         return this.architecture.get();
     }
 
-//    @Override
-//    public String toString() {
-//        return configuration.getSingleFile().toString();
-//    }
-
-
     @Override
     public String toString() {
-        return "ElasticsearchDistribution{" +
-            "name='" + name + '\'' +
-            ", dockerSupport=" + dockerSupport +
-            ", configuration=" + configuration +
-            ", extracted=" + extracted +
-            ", architecture=" + architecture +
-            ", version=" + version +
-            ", type=" + type +
-            ", platform=" + platform +
-            ", flavor=" + flavor +
-            ", bundledJdk=" + bundledJdk +
-            ", failIfUnavailable=" + failIfUnavailable +
-            '}';
+        return configuration.getSingleFile().toString();
     }
 
     public Extracted getExtracted() {
