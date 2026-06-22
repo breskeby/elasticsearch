55eff078ead71a3491e0474489a1a699a38eefbc
ChrisHegarty <chegar999@gmail.com>
2026-06-22 13:27:29 +0100

    forbidden for 22

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/precommit/ForbiddenApisPrecommitPlugin.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/precommit/ForbiddenApisPrecommitPlugin.java
@@ -47,6 +47,7 @@ public class ForbiddenApisPrecommitPlugin extends PrecommitPlugin {
             t.copy("forbidden/http-signatures.txt");
             t.copy("forbidden/es-server-signatures.txt");
             t.copy("forbidden/jdk-foreign-signatures.txt");
+            t.copy("forbidden/jdk-foreign-signatures22.txt");
         });
 
         project.getExtensions().getByType(SourceSetContainer.class).configureEach(sourceSet -> {
